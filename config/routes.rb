863884bc4478a531818b91198a3ea8c57db8d6b8
Charles Washington <charles_was_7@hotmail.com>
2019-07-31 17:44:17 -0300

    Improve external UI. Apply SRT theme to login, sign up, forgot password views.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'dashboard/index'
   resources :resources
 
-  root to: "home#index"
+  root to: 'dashboard#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
